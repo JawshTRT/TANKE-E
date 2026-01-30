9d8913da65bd5a0dcf7779c3e2911ba531f200b3
Josh Howery <joshowery@gmail.com>
2026-01-29 20:02:11 -0500

    starting using example code from tungstenite-rs for sending JSON massages

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,17 +2,25 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
-
+use tungstenite::{Message, connect};
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+    //Probably an environment logger?
+
     let ui = AppWindow::new()?;
 
     ui.on_connect({
         let ui_handle = ui.as_weak();
+        //Port and ip arguments lie within the pipe operator of move
         move |port, ip| {
             let ui = ui_handle.unwrap();
             // TODO: Connect to robot server
+
+            let (mut socket, response) =
+                connect("ws://someip:someport/socket").expect("Can't connect");
+
             println!("Connected to port {0} at ip {1}", port, ip);
         }
     });
